70a4bb7a3e90031df52bd81396c44ae024054304
nano155 <arq.molina@live.com>
2024-01-16 17:12:22 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+En la carpeta de entregables, estan los archivos js entregables segun la actividad requerida, en este caso seria el test-1.js en el cual se contamos con 2 clases, las cuales cumplen con las condiciones requeridas en la consigna.
---
+++ entregable-1/test-1.js
@@ -0,0 +1,65 @@
+const { v4 : uuidv4 } = require("uuid")
+class Product {
+    constructor(title, description, price, thumbnail, code, stock){
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.thumbnail = thumbnail;
+        this.code = code;
+        this.stock = stock
+        this.id = uuidv4()
+
+    }
+}
+
+class ProductManager  {
+    constructor(){
+        this.products = []
+    }
+    getProducts=()=>{
+
+        return this.products
+    }
+    addProduct =(producto)=>{
+        const product = {
+            title : producto.title,
+            description: producto.description,
+            price: producto.price,
+            thumbnail: producto.thumbnail,
+            code: producto.code,
+            stock: producto.stock,
+            id: producto.id
+        }
+        const existe = this.products.some(item => item.code === product.code)
+        
+        if(!existe){
+            this.products.push(product)
+        } else{
+            console.error('el codigo esta repetido')
+        }
+    }
+    getProductById = (id) =>{
+
+        return this.products.some(item => item.id === id) ? this.products.filter(item => item.id === id ) :'No existe el producto'
+    }
+
+}
+
+const admin = new ProductManager()
+
+const producto1 = new Product('producto prueba', 'Este es un producto', 200, 'Sin Imagen', 'abc123', 25)
+
+const producto2 = new Product('producto prueba', 'Este es un producto', 200, 'Sin Imagen', 'abc1234', 25)
+
+console.log(admin.getProducts());
+
+admin.addProduct(producto1);
+
+admin.addProduct(producto2);
+
+console.log(admin.getProducts());
+console.log(admin.getProductById(producto2.id));
+console.log(admin.getProductById(20));
+
+
+

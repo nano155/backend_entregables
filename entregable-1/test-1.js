16ef256ef3e51c9f9bdf7a0fa35045d5c951f185
nano155 <arq.molina@live.com>
2024-01-21 12:07:52 -0500

    correccion

--- entregable-1/test-1.js
+++ entregable-1/test-1.js
@@ -1,71 +1,82 @@
-const { v4 : uuidv4 } = require("uuid")
 class Product {
-    constructor(title, description, price, thumbnail, code, stock){
-        this.title = title;
-        this.description = description;
-        this.price = price;
-        this.thumbnail = thumbnail;
-        this.code = code;
-        this.stock = stock
-        this.id = uuidv4()
-
-    }
+  static id = 1;
+  constructor(title, description, price, thumbnail, code, stock) {
+    this.title = title;
+    this.description = description;
+    this.price = price;
+    this.thumbnail = thumbnail;
+    this.code = code;
+    this.stock = stock;
+    this.id = Product.id++;
+  }
 }
 
-class ProductManager  {
-    constructor(){
-        this.products = []
-    }
-    static id = 0
+class ProductManager {
+  constructor() {
+    this.products = [];
+  }
+  static id = 0;
 
-    getProducts=()=>{
-
-        return this.products
+  getProducts = () => {
+    return this.products;
+  };
+  addProduct = (producto) => {
+    const product = {
+      title: producto.title,
+      description: producto.description,
+      price: producto.price,
+      thumbnail: producto.thumbnail,
+      code: producto.code,
+      stock: producto.stock,
+      id: producto.id,
+    };
+    const {title, description, code, id, price, stock, thumbnail} = product
+    const existe = this.products.some((item) => item.code === product.code);
+    if (!existe) {
+      if (
+        title  &&
+        description  &&
+        price  &&
+        thumbnail  && 
+        code  && 
+        stock  &&
+        id 
+      ){
+        this.products.push(product)
+      }else{
+        console.log('Todos los campos deben estar definidos');
+      }
+    } else {
+      console.error("el codigo esta repetido");
     }
-    addProduct =(producto)=>{
-        const product = {
-            title : producto.title,
-            description: producto.description,
-            price: producto.price,
-            thumbnail: producto.thumbnail,
-            code: producto.code,
-            stock: producto.stock,
-            id: ProductManager.id++
-        }
-        const existe = this.products.some(item => item.code === product.code)
-        
-        if(!existe){
-            this.products.push(product)
-        } else{
-            console.error('el codigo esta repetido')
-        }
-    }
-    getProductById = (id) =>{
+  };
+  getProductById = (id) => {
 
-        return this.products.some(item => item.id === id) ? this.products.filter(item => item.id === id ) :'No existe el producto'
-    }
-
+   return (this.products.find(producto => producto.id === id ) || Error('No se encontro el id'));
+  };
 }
 
-const admin = new ProductManager()
+const admin = new ProductManager();
 
-const producto1 = new Product('producto prueba', 'Este es un producto', 200, 'Sin Imagen', 'abc123', 25)
+const producto1 = new Product(
+  'Producto prueba',
+  "Este es un producto",
+  200,
+  "Sin Imagen",
+  "abc123",
+  25
+);
 
-const producto2 = new Product('producto prueba', 'Este es un producto', 200, 'Sin Imagen', 'abc1234', 25)
 
-const producto3 = new Product('producto prueba', 'Este es un producto', 200, 'Sin Imagen', 'abc12345', 25)
+
 
 console.log(admin.getProducts());
 
 admin.addProduct(producto1);
 
-admin.addProduct(producto2);
-
-admin.addProduct(producto3);
 
 console.log(admin.getProducts());
-console.log(admin.getProductById(2));
-console.log(admin.getProductById(20));
 
+console.log(admin.getProductById(1));
 
 
